7cf220666126483a65a003f2aca2ec7d4bc37a89
ShinYooJung <shinyoojung0@gmail.com>
2023-05-28 14:53:46 +0900

    first commit

---
+++ src/main/java/com/example/cnualarm/CnuAlarmBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cnualarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnuAlarmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnuAlarmBeApplication.class, args);
+    }
+
+}
