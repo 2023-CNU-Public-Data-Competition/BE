0ea98f5de4e3b39e58ce31175def2a1e3ba714e0
Yujin-nKim <yujinkimvv@gmail.com>
2023-06-10 06:02:40 +0900

    api 수정

--- src/main/java/com/example/cnualarm/Alarm/AlarmService.java
+++ src/main/java/com/example/cnualarm/Alarm/AlarmService.java
@@ -23,6 +23,19 @@ public class AlarmService {
     @Autowired
     AlarmSettingRepository alarmSettingRepository;
 
+    public List<AlarmsDTO> getNewAlarms(String user_id) {
+        Collection<AlarmEntity> alarmEntities = alarmRepository.findByuserId(user_id);
+        List<AlarmsDTO> alarmsDTOList = new ArrayList<>();
+
+        for (AlarmEntity alarmEntity : alarmEntities) {
+            if (alarmEntity.getIsAlarm().equals("NO")) {
+                AlarmsDTO alarmsDTO = EntityConverter.alarmsToDTO(alarmEntity);
+                alarmsDTOList.add(alarmsDTO);
+            }
+        }
+        return alarmsDTOList;
+    }
+
     public List<AlarmsDTO> getAlarms(String id) {
         Collection<AlarmEntity> alarmEntities = alarmRepository.findByuserId(id);
         List<AlarmsDTO> alarmsDTOList = new ArrayList<>();
@@ -33,8 +46,8 @@ public class AlarmService {
         return alarmsDTOList;
     }
 
-    public List getAlarmsSettings(String id, String alarm_type) {
-        List<AlarmSettingEntity> Result = alarmSettingRepository.findByUserIdAndAlarmType(id, alarm_type);
+    public List getAlarmsSettings(String user_id, String alarm_type) {
+        List<AlarmSettingEntity> Result = alarmSettingRepository.findByUserIdAndAlarmType(user_id, alarm_type);
         List return_list = new ArrayList();
         for(AlarmSettingEntity item : Result) {
             return_list.add(item.getAlarmName());
@@ -43,15 +56,11 @@ public class AlarmService {
         return return_list;
     }
 
-    public String updateAlarmSetting(AlarmSetingDTO alarmSetingDTO) {
-        String id = alarmSetingDTO.getId();
-        String alarmType = alarmSetingDTO.getAlarmType();
-        List<String> alarmNames = alarmSetingDTO.getList();
-
-        System.out.println(id);
-        System.out.println(alarmType);
-        System.out.println(alarmNames);
-
-        return "a";
-    }
+//    public String updateAlarmSetting(AlarmSetingDTO alarmSetingDTO) {
+//        String id = alarmSetingDTO.getId();
+//        String alarmType = alarmSetingDTO.getAlarmType();
+//        List<String> alarmNames = alarmSetingDTO.getList();
+//
+//        return "a";
+//    }
 }
